ec6bb3658eb6d623384cef1ffc988e24f5398694
sebastienserre <sebastien@thivinfo.com>
2018-07-08 18:47:45 +0200

    add comment

--- pro/shortcode/class-openagenda-embed-shortcode.php
+++ pro/shortcode/class-openagenda-embed-shortcode.php
@@ -20,8 +20,19 @@ class Openagenda_Embed_Shortcode {
 		add_shortcode( 'openagenda_embed', array( $this, 'openwp_pro_shortcode' ) );
 	}
 
+	/**
+	 * Display Openagenda embeds with a WordPress Shortcode.
+	 * [openagenda_embed url="https://openagenda.com/iledefrance" lang="fr" widget="map"]
+	 * url => string with URL of OpenAgenda agenda. (required).
+	 * lang => language to display (2 letters country code: en/fr...). default: en. optional.
+	 * widget => Openagenda widget to display. Possible settings: general, map, search, categories, tags, calendrier, preview.
+	 *
+	 * @param array $atts Shortcode params.
+	 *
+	 * @return string
+	 */
 	public function openwp_pro_shortcode( $atts ) {
-		$atts = shortcode_atts( array(
+		$atts   = shortcode_atts( array(
 			'url'    => 'test',
 			'widget' => 'general',
 			'lang'   => 'en',
