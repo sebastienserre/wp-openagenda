5fc5ef8bf17785fa779981b01695e2904cb2f4cb
sebastienserre <sebastien@thivinfo.com>
2018-07-13 16:08:09 +0200

    create a directory with all WP Widgets

--- wp-openagenda.php
+++ wp-openagenda.php
@@ -81,8 +81,7 @@ if ( ! function_exists( 'openagenda_fs' ) ) {
 			include_once THFO_OPENWP_PLUGIN_PATH . '/pro/vc/class-openagenda-slider.php';
 		}
 
-		include_once THFO_OPENWP_PLUGIN_PATH . '/pro/class-openwp-main-widget.php';
-		include_once THFO_OPENWP_PLUGIN_PATH . '/pro/class-openagenda-main-widget.php';
+		include_once THFO_OPENWP_PLUGIN_PATH . '/pro/widget/class-openagenda-main-widget.php';
 		include_once THFO_OPENWP_PLUGIN_PATH . '/pro/shortcode/class-openagenda-embed-shortcode.php';
 		include_once THFO_OPENWP_PLUGIN_PATH . '/pro/shortcode/class-openagendaslidershortcode.php';
 	}
