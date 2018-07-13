c6fed0d8e111015d20c0f607089fd2e42f491bde
sebastienserre <sebastien@thivinfo.com>
2018-07-13 13:26:14 +0200

    correct Jquery

--- assets/js/openagenda_slick.js
+++ assets/js/openagenda_slick.js
@@ -1,7 +1,7 @@
-$(document).ready(function () {
+jQuery(document).ready(function ($) {
     $('.bloc-openagenda__slider').slick({
         infinite: true,
-        autoplay: false,
+        /*autoplay: true,*/
         arrows: true,
         cssEase: 'linear',
         adaptiveHeight: true
@@ -9,13 +9,13 @@ $(document).ready(function () {
     ;
 });
 
-/*
-$('.bloc-openagenda__slide').each(function (idx, item) {
+
+jQuery('.bloc-openagenda__slide').each(function (idx, item) {
     var carouselId = "carousel" + idx;
     this.id = carouselId;
-    $(this).slick({
+    jQuery(this).slick({
         slide: "#" + carouselId +" .slide",
        appendArrows: "#" + carouselId + " .arrows-bottom",
 
     });
-});*/
+});
