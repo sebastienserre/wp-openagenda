15cd0d662e6ea53f4bd21353bb2e29f63653dd82
sebastienserre <sebastien@thivinfo.com>
2018-07-13 14:03:44 +0200

    basic slider JS OK

--- assets/js/openagenda_slick.js
+++ assets/js/openagenda_slick.js
@@ -1,22 +1,10 @@
 jQuery(document).ready(function ($) {
     $('.bloc-openagenda__slider').slick({
         infinite: true,
-        /*autoplay: true,*/
-        /*arrows: true,*/
+        autoplay: true,
         dots: true,
         cssEase: 'linear',
         adaptiveHeight: true
     })
     ;
-});
-
-
-jQuery('.bloc-openagenda__slide').each(function (idx, item) {
-    var carouselId = "carousel" + idx;
-    this.id = carouselId;
-    jQuery(this).slick({
-        slide: "#" + carouselId +" .slide",
-       appendArrows: "#" + carouselId + " .arrows-bottom",
-
-    });
 });
