e053ec409e94fee1b4a4ca82e6ddd01d67959d1c
sebastienserre <sebastien@thivinfo.com>
2018-12-26 18:11:54 +0100

    add pro Gutenblock (embed)

--- wp-openagenda.php
+++ wp-openagenda.php
@@ -117,6 +117,7 @@ if ( ! function_exists( 'openagenda_fs' ) ) {
 			include_once THFO_OPENWP_PLUGIN_PATH . '/pro/shortcode/class-openagenda-embed-shortcode.php';
 			include_once THFO_OPENWP_PLUGIN_PATH . '/pro/shortcode/class-openagendaslidershortcode.php';
 			include_once THFO_OPENWP_PLUGIN_PATH . '/pro/shortcode/class-openagenda-search-shortcode.php';
+			include_once THFO_OPENWP_PLUGIN_PATH . '/pro/blocks/class-openwp-block-embed.php';
 		}
 
 		/**
