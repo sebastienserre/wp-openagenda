002ef7ae4b8a9776f10d10bce3b7f37479d159e5
sebastienserre <sebastien@thivinfo.com>
2019-07-03 18:48:15 +0200

    add a force sync button

--- pro/inc/helpers.php
+++ pro/inc/helpers.php
@@ -9,3 +9,10 @@ function openwp_launch_import_on_new_agenda( $term_id, $tt_id ) {
 	$agenda = $agenda->name;
 	import_oa_events__premium_only( $agenda );
 }
+
+add_action( 'admin_init', 'openwp_sync_from_admin' );
+function openwp_sync_from_admin(){
+	if ( ! empty( $_GET['sync'] && 'now' === $_GET[ 'sync'] && wp_verify_nonce( $_GET['_wpnonce'], 'force_sync' ) ) ){
+		import_oa_events__premium_only();
+	}
+}
