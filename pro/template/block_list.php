1556a173e0f1b94e72635a1642b1ffbcde839cc6
sebastienserre <sebastien@thivinfo.com>
2019-07-04 18:32:42 +0200

    add template

---
+++ pro/template/block_list.php
@@ -0,0 +1,57 @@
+<?php
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+} // Exit if accessed directly.
+?>
+<div class="openagenda-event-list">
+	<?php
+	if ( has_post_thumbnail( $event['id'] ) ) {
+		?>
+        <div class="event-thumbnail">
+			<?php
+			echo get_the_post_thumbnail( $event['id'], 'post-thumbnail' );
+			?>
+        </div>
+		<?php
+	}
+	?>
+
+    <h2><?php echo get_the_title( $event['id'] ); ?></h2>
+    <p>
+		<?php
+		echo get_the_content( '', '', $event['id'] );;
+		?>
+    </p>
+    <div class="meta">
+		<?php
+		echo openwp_display_date( $start, $end );
+		?>
+		<?php
+		if ( ! empty( $conditions ) ) { ?>
+            <h3>
+				<?php _e( 'Conditions of participation, rates', 'wp-openagenda' ); ?>
+            </h3>
+            <p><?php echo $conditions; ?></p>
+			<?php
+		}
+		?>
+		<?php
+		if ( ! empty( $tools ) ) { ?>
+            <h3>
+				<?php _e( 'Registration tools', 'wp-openagenda' ); ?>
+            </h3>
+            <p><?php echo $tools; ?></p>
+			<?php
+		}
+		?>
+		<?php
+		if ( ! empty( $min_age ) || ! empty( $max_age ) ) { ?>
+            <h3>
+				<?php _e( 'Age', 'wp-openagenda' ); ?>
+            </h3>
+            <p><?php echo openwp_display_age( $min_age, $max_age ); ?></p>
+			<?php
+		}
+		?>
+    </div>
+</div>
