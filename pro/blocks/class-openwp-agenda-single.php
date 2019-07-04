79c195100455b632b56568958c8276cb7607f766
sebastienserre <sebastien@thivinfo.com>
2019-07-04 16:04:30 +0200

    add single blocks

---
+++ pro/blocks/class-openwp-agenda-single.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace WPGC\BlOCKS\Single;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+} // Exit if accessed directly.
+
+class OA_Single {
+	public function __construct() {
+	}
+
+	public function block_init() {
+
+	}
+}
+new OA_Single();
