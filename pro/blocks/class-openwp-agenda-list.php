6d18963980b9223567eff9f99bdb99e846bd243d
sebastienserre <sebastien@thivinfo.com>
2019-07-04 17:25:55 +0200

    display date

--- pro/blocks/class-openwp-agenda-list.php
+++ pro/blocks/class-openwp-agenda-list.php
@@ -4,7 +4,12 @@ namespace WPGC\BlOCKS\Single;
 
 use Carbon_Fields\Field;
 use Carbon_Fields\Block;
+use function carbon_get_post_meta;
+use function date_i18n;
+use function get_the_content;
+use function get_the_post_thumbnail;
 use function get_the_title;
+use function has_post_thumbnail;
 use function var_dump;
 
 if ( ! defined( 'ABSPATH' ) ) {
@@ -18,29 +23,56 @@ class OA_Event_List {
 
 	public function block_init() {
 		Block::make( __( 'Openagenda Single', 'wp-openagenda' ) )
-			->set_category( 'custom-category', 'Openagenda', 'calendar-alt' )
-			->set_render_callback( array( $this, 'render' ) )
-			->add_fields(
-				[
-					Field::make( 'association', 'openwp_event_association', __( 'Event', 'wp-openagenda' ) )
-					->set_types(
-						[
-							[
-								'type' =>   'post',
-								'post_type' =>  'openagenda-events'
-							]
-						]
-					),
-				]
-			);
+		     ->set_category( 'custom-category', 'Openagenda', 'calendar-alt' )
+		     ->set_render_callback( array( $this, 'render' ) )
+		     ->add_fields(
+			     [
+				     Field::make( 'association', 'openwp_event_association', __( 'Event', 'wp-openagenda' ) )
+				          ->set_types(
+					          [
+						          [
+							          'type'      => 'post',
+							          'post_type' => 'openagenda-events'
+						          ]
+					          ]
+				          ),
+			     ]
+		     );
 	}
 
-	public function render( $block ){
-		var_dump( $block );
+	public function render( $block ) {
 
-		foreach ( $block['openwp_event_association'] as $event ){
+		foreach ( $block['openwp_event_association'] as $event ) {
 			?>
-			<h2><?php echo get_the_title( $event['id'] ); ?></h2>
+            <div class="openagenda-event-list">
+				<?php
+				if ( has_post_thumbnail( $event['id'] ) ) {
+					?>
+                    <div class="event-thumbnail">
+						<?php
+						echo get_the_post_thumbnail( $event['id'], 'post-thumbnail' );
+						?>
+                    </div>
+					<?php
+				}
+				?>
+
+                <h2><?php echo get_the_title( $event['id'] ); ?></h2>
+                <p>
+					<?php
+					echo get_the_content( '', '', $event['id'] );;
+					?>
+                </p>
+                <div class="meta">
+                    <?php
+                    $start = date_i18n( 'd F Y', carbon_get_post_meta( $event['id'], 'oa_start') );
+                    $end = date_i18n( 'd F Y', carbon_get_post_meta( $event['id'], 'oa_end' ) );
+                    printf( __( 'From %1$s to %2$s', 'wp-openagenda' ), $start, $end );
+                    ?>
+
+                </div>
+
+            </div>
 			<?php
 		}
 
@@ -49,4 +81,5 @@ class OA_Event_List {
 		<?php
 	}
 }
+
 new OA_Event_List();
