82d06a034c71222428ebd6ec615d38d821739fa6
sebastienserre <sebastien@thivinfo.com>
2019-07-25 16:59:06 +0200

    allow front to cpt

--- pro/inc/cpt.php
+++ pro/inc/cpt.php
@@ -55,12 +55,12 @@ if ( ! function_exists('openagenda_event') && 'yes' !== $tec ) {
 			'show_in_admin_bar'     => true,
 			'show_in_nav_menus'     => false,
 			'can_export'            => true,
-			'has_archive'           => false,
-			'exclude_from_search'   => true,
-			'publicly_queryable'    => false,
+			'has_archive'           => true,
+			'exclude_from_search'   => false,
+			'publicly_queryable'    => true,
 			'capability_type'       => 'page',
-			'rewrite'               => false,
-			'show_in_rest'               => false,
+			'rewrite'               => true,
+			'show_in_rest'               => true,
 		);
 		register_post_type( 'openagenda-events', $args );
 
