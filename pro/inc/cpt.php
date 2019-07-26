bd67165569029e1340f611d47f66a33c842f79bb
sebastienserre <sebastien@thivinfo.com>
2019-07-26 09:07:36 +0200

    add some styles

--- pro/inc/cpt.php
+++ pro/inc/cpt.php
@@ -55,11 +55,11 @@ if ( ! function_exists('openagenda_event') && 'yes' !== $tec ) {
 			'show_in_admin_bar'     => true,
 			'show_in_nav_menus'     => true,
 			'can_export'            => true,
-			'has_archive'           => false,
-			'exclude_from_search'   => true,
-			'publicly_queryable'    => false,
+			'has_archive'           => true,
+			'exclude_from_search'   => false,
+			'publicly_queryable'    => true,
 			'capability_type'       => 'page',
-			'show_in_rest'               => false,
+			'show_in_rest'               => true,
 			'rewrite'               => true,
 		);
 		register_post_type( 'openagenda-events', $args );
