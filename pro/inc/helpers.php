00c7ff86e5017c6c6d4d0e5bacda7e0f95c5f4ee
sebastienserre <sebastien@thivinfo.com>
2019-07-26 19:08:08 +0200

    remove notice

--- pro/inc/helpers.php
+++ pro/inc/helpers.php
@@ -12,7 +12,7 @@ function openwp_launch_import_on_new_agenda( $term_id, $tt_id ) {
 
 add_action( 'admin_init', 'openwp_sync_from_admin' );
 function openwp_sync_from_admin(){
-	if ( ! empty( $_GET['sync'] && 'now' === $_GET[ 'sync'] && wp_verify_nonce( $_GET['_wpnonce'], 'force_sync' ) ) ){
+	if ( ! empty( $_GET['sync']  )&& 'now' === $_GET[ 'sync'] && wp_verify_nonce( $_GET['_wpnonce'], 'force_sync' ) ){
 		import_oa_events__premium_only();
 	}
 }
