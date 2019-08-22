6428e8f95d166ef1a443d09c508f2862743f9465
sebastienserre <sebastien@thivinfo.com>
2019-08-22 10:56:13 +0200

    update Carbon fields & Parsedown

--- pro/inc/custom-fields.php
+++ pro/inc/custom-fields.php
@@ -19,59 +19,58 @@ function oa_create_custom_fields() {
 	);
 
 	Container::make( 'post_meta', 'OpenAgenda Data' )
-	         ->where( 'post_type', '=', 'openagenda-events' )
-	         ->add_fields(
-		         array(
-			         Field::make( 'text', 'oa_conditions', __( 'Conditions of participation, rates', 'wp-openagenda' ) ),
-		         )
-	         )
-	         ->add_fields(
-		         array(
-			         Field::make( 'text', 'oa_tools', __( 'Registration tools', 'wp-openagenda' ) )
-				         ->set_attribute( 'placeholder', __( 'URL or email', 'wp-openagenda' ) ),
-		         )
-	         )
-	         ->add_fields(
-		         array(
-			         Field::make( 'select', 'oa_min_age', __( 'Minimum age', 'wp-openagenda' ) )
-			              ->set_options( oa_age() )
-			              ->set_width( 50 ),
-		         )
-	         )
-	         ->add_fields(
-		         array(
-			         Field::make( 'select', 'oa_max_age', __( 'Maximum age', 'wp-openagenda' ) )
-			              ->set_options( oa_age() )
-			              ->set_width( 50 ),
-		         )
-	         )
-	         ->add_fields(
-		         array(
-			         Field::make( 'multiselect', 'oa_a11y', __( 'Accessibility', 'wp-openagenda' ) )
-			              ->add_options( $accessibility ),
-		         )
-	         )
-	         ->add_fields(
-		         array(
-			         Field::make( 'date_time', 'oa_start', __( 'start date', 'wp-openagenda' ) )
-			              ->set_storage_format( 'U' )
-			              ->set_attribute( 'placeholder', __( 'Start date for this event', 'wp-openagenda' ) )
-			              ->set_picker_options( [ 'allowInput' => false ] )
-			              ->set_width( 50 )
-		->set_required( true ),
-		         )
-	         )
-	         ->add_fields(
-		         array(
+		->where( 'post_type', '=', 'openagenda-events' )
+		->add_fields(
+			[
+				Field::make( 'text', 'oa_conditions', __( 'Conditions of participation, rates', 'wp-openagenda' ) ),
+			]
+		)
+		->add_fields(
+			[
+				Field::make( 'text', 'oa_tools', __( 'Registration tools', 'wp-openagenda' ) )
+					->set_attribute( 'placeholder', __( 'URL or email', 'wp-openagenda' ) ),
+			]
+		)
+		->add_fields(
+			[
+				Field::make( 'select', 'oa_min_age', __( 'Minimum age', 'wp-openagenda' ) )
+					->set_options( oa_age() )
+					->set_width( 50 ),
+			]
+		)
+		->add_fields(
+			[
+				Field::make( 'select', 'oa_max_age', __( 'Maximum age', 'wp-openagenda' ) )
+					->set_options( oa_age() )
+					->set_width( 50 ),
+			]
+		)
+		->add_fields(
+			[
+				Field::make( 'multiselect', 'oa_a11y', __( 'Accessibility', 'wp-openagenda' ) )
+					->add_options( $accessibility ),
+			]
+		)
+		->add_fields(
+			Field::make( 'complex', 'oa_event_date', __( 'Event', 'wp-openagenda' ) )
+				->add_fields( 'date',
+					[
+						Field::make( 'date_time', 'oa_start', __( 'start date', 'wp-openagenda' ) )
+							->set_storage_format( 'U' )
+							->set_attribute( 'placeholder', __( 'Start date for this event', 'wp-openagenda' ) )
+							->set_picker_options( [ 'allowInput' => false ] )
+							->set_width( 50 )
+							->set_required( true ),
+						Field::make( 'date_time', 'oa_end', __( 'End date', 'wp-openagenda' ) )
+							->set_storage_format( 'U' )
+							->set_attribute( 'placeholder', __( 'End date for this event', 'wp-openagenda' ) )
+							->set_picker_options( [ 'allowInput' => false ] )
+							->set_width( 50 )
+							->set_required( true ),
+					]
+				)
+		);
 
-			         Field::make( 'date_time', 'oa_end', __( 'End date', 'wp-openagenda' ) )
-			              ->set_storage_format( 'U' )
-			              ->set_attribute( 'placeholder', __( 'End date for this event', 'wp-openagenda' ) )
-			              ->set_picker_options( [ 'allowInput' => false ] )
-			              ->set_width( 50 )
-				         ->set_required( true ),
-		         )
-	         );
 
 }
 
