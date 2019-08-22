73bcd12611c8f6f7c07759063b66caa656eab51d
sebastienserre <sebastien@thivinfo.com>
2019-08-22 14:51:50 +0200

    add AcF Fields

--- pro/main.php
+++ pro/main.php
@@ -2,13 +2,13 @@
 
 namespace OPENWP\Pro\Main;
 
+use function add_action;
 use function basename;
 use function class_exists;
 use function dirname;
 use function load_plugin_textdomain;
 use function wp_enqueue_style;
 use function wp_register_script;
-use const __FILE__;
 use const THFO_OPENWP_PLUGIN_PATH;
 use const THFO_OPENWP_PLUGIN_URL;
 
@@ -17,17 +17,30 @@ if ( ! defined( 'ABSPATH' ) ) {
 } // Exit if accessed directly.
 
 
-class Main {
+class MainPro {
 
 	public function __construct() {
-		add_action( 'plugins_loaded', [ $this, 'load_files'] );
-		add_action( 'wp_enqueue_scripts', array( $this, 'openwp_pro_load_style__premium_only' ) );
-		add_action( 'plugins_loaded', array( $this, 'openwp_load_textdomain__premium_only' ) );
+		add_action( 'plugins_loaded', [ $this, 'define_premium_constants' ], 999 );
+		add_action( 'plugins_loaded', [ $this, 'load_files' ], 999 );
+		add_action( 'wp_enqueue_scripts', array( $this, 'openwp_pro_load_style__premium_only' ), 999 );
+		add_action( 'plugins_loaded', array( $this, 'openwp_load_textdomain__premium_only' ), 999 );
 		add_action( 'wp_enqueue_scripts', array( $this, 'openwp_register_script__premium_only' ), 999 );
-		add_action( 'admin_print_styles', array( $this, 'openwp_admin_style__premium_only') );
+		add_action( 'admin_print_styles', array( $this, 'openwp_admin_style__premium_only' ), 999 );
+
+		add_filter( 'acf/settings/url', [ $this, 'my_acf_settings_url' ], 999 );
+		//add_filter('acf/settings/show_admin', [ $this, 'my_acf_settings_show_admin' ] );
 	}
 
-	public function load_files(){
+	public function define_premium_constants() {
+
+		// Define path and URL to the ACF plugin.
+		define( 'OPENWP_PRO_PATH', THFO_OPENWP_PLUGIN_PATH . 'pro/' );
+		define( 'OPENWP_PRO_URL', THFO_OPENWP_PLUGIN_URL . 'pro/' );
+		define( 'MY_ACF_PATH', OPENWP_PRO_PATH . '/3rd-party/acf/' );
+		define( 'MY_ACF_URL', OPENWP_PRO_URL . '/3rd-party/acf/' );
+	}
+
+	public function load_files() {
 		/**
 		 * If Visual Composer activated, load VC elements.
 		 */
@@ -52,7 +65,18 @@ class Main {
 		include_once THFO_OPENWP_PLUGIN_PATH . '/pro/blocks/class-openwp-block-embed.php';
 		include_once THFO_OPENWP_PLUGIN_PATH . '/pro/cronjob.php';
 		include_once THFO_OPENWP_PLUGIN_PATH . '/pro/inc/helpers.php';
-		include_once THFO_OPENWP_PLUGIN_PATH . '/pro/blocks/class-openwp-agenda-list.php';
+		include_once THFO_OPENWP_PLUGIN_PATH . 'pro/blocks/class-openwp-agenda-list.php';
+
+		include_once MY_ACF_PATH . 'acf.php';
+		include_once OPENWP_PRO_PATH . 'inc/acf-fields.php';
+	}
+
+	public function my_acf_settings_url( $url ) {
+		return MY_ACF_URL;
+	}
+
+	public function my_acf_settings_show_admin( $show_admin ) {
+		return false;
 	}
 
 	/**
@@ -74,7 +98,7 @@ class Main {
 				'jquery-ui-datepicker',
 			)
 		);
-		wp_enqueue_style( 'openwp-pro', THFO_OPENWP_PLUGIN_URL . 'pro/assets/css/openwp-pro.css', array('slickthemecss') );
+		wp_enqueue_style( 'openwp-pro', THFO_OPENWP_PLUGIN_URL . 'pro/assets/css/openwp-pro.css', array( 'slickthemecss' ) );
 		wp_register_script( 'IsotopeOA', THFO_OPENWP_PLUGIN_URL . 'pro/assets/js/isotope.pkgd.min.js',
 			array(
 				'jquery',
@@ -87,7 +111,7 @@ class Main {
 		);
 	}
 
-	public function openwp_admin_style__premium_only(){
+	public function openwp_admin_style__premium_only() {
 		wp_enqueue_style( 'openwp-pro', THFO_OPENWP_PLUGIN_URL . 'pro/assets/css/openwp-pro.css' );
 	}
 
@@ -99,4 +123,5 @@ class Main {
 		wp_enqueue_style( 'jquery-ui-dp', THFO_OPENWP_PLUGIN_URL . 'pro/assets/css/jquery-ui.min.css' );
 	}
 }
-new namespace\Main();
+
+new MainPro();
