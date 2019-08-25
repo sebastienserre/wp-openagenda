de62f6a9e3dcb1f33fc9ce0b32d0b33d10677270
sebastienserre <sebastien@thivinfo.com>
2019-08-25 16:28:11 +0200

    code reformat

--- pro/template/single.php
+++ pro/template/single.php
@@ -12,70 +12,67 @@ get_header();
     <!-- Start Openagenda Single -->
     <section id="primary" class="content-area oa-content-area">
         <main id="main" class="site-main">
-            <?php
-            if ( have_posts() ) {
-	            while ( have_posts() ) {
-		            the_post();
-
-		            ?>
+			<?php
+			if ( have_posts() ) {
+				while ( have_posts() ) {
+					the_post();
+					?>
                     <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
                         <header class="oa-entry-header">
-				            <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+							<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
                         </header>
                         <div class="oa-content">
                             <aside>
                                 <p class="oa-date">
-						            <?php
-
-						            _e( 'Date : ', 'wp-openagenda' );
-						            echo openwp_display_date( get_the_ID() );
-
-						            ?>
+									<?php
+									_e( 'Date : ', 'wp-openagenda' );
+									echo openwp_display_date( get_the_ID() );
+									?>
                                 </p>
                                 <p class="oa-age">
-						            <?php
-						            _e( 'Public : ', 'wp-openagenda' );
-						            echo openwp_display_age( get_the_ID() );
+									<?php
+									_e( 'Public : ', 'wp-openagenda' );
+									echo openwp_display_age( get_the_ID() );
 
-						            ?>
+									?>
                                 </p>
-					            <?php
-					            /**
-					             * Display acessibility
-					             */
-					            echo openwp_display_accessibilty( get_the_ID() );
-					            ?>
+								<?php
+								/**
+								 * Display acessibility
+								 */
+								echo openwp_display_accessibilty( get_the_ID() );
+								?>
 
                             </aside>
                             <div class="oa-content">
                                 <div class="oa-description">
-						            <?php the_content(); ?>
+									<?php the_content(); ?>
                                 </div>
                                 <div class="oa-condition">
-						            <?php echo esc_attr( $condition ); ?>
+									<?php echo esc_attr( $condition ); ?>
                                 </div>
                                 <div class="oa-tool">
-						            <?php
-						            if ( filter_var( $tool, FILTER_VALIDATE_URL ) ) {
-							            ?>
+									<?php
+									if ( filter_var( $tool, FILTER_VALIDATE_URL ) ) {
+										?>
                                         <a href="<?php echo $tool; ?>"><?php echo $tool; ?></a>
-							            <?php
-						            } elseif ( is_email( $tool ) ) {
-							            ?>
+										<?php
+									} elseif ( is_email( $tool ) ) {
+										?>
                                         <a href="mailto:<?php echo antispambot( $tool ); ?> "><?php echo antispambot( $tool );
-								            ?></a>
-							            <?php
-						            } else {
-							            echo '<p>' . esc_attr( $tool ) . '</p>';
-						            } ?>
+											?></a>
+										<?php
+									} else {
+										echo '<p>' . esc_attr( $tool ) . '</p>';
+									} ?>
                                 </div>
                             </div>
                         </div>
                     </article>
-		            <?php
-	            }
-            }
-                ?>
+					<?php
+				}
+			}
+			?>
         </main>
     </section> <!-- Primary -->
     <!-- End Openagenda Single -->
