5ba5f1cc69d6b623f1468405c11dad09498bef62
sebastienserre <sebastien@thivinfo.com>
2019-10-21 17:52:55 +0200

    export event to OA from TEC

--- pro/inc/class-the-event-calendar.php
+++ pro/inc/class-the-event-calendar.php
@@ -13,7 +13,9 @@ use function array_pop;
 use function array_push;
 use function array_reverse;
 use function class_exists;
+use function curl_exec;
 use function curl_init;
+use function curl_setopt;
 use function date;
 use function esc_attr_e;
 use function esc_url_raw;
@@ -21,13 +23,15 @@ use function explode;
 use function extract;
 use function get_post_meta;
 use function get_post_type;
+use function get_the_post_thumbnail_url;
 use function get_the_terms;
 use function implode;
 use function intval;
-use function json_encode;
+use function rand;
 use function sanitize_email;
 use function sanitize_text_field;
 use function sizeof;
+use function strtotime;
 use function tribe_create_event;
 use function tribe_create_organizer;
 use function tribe_create_venue;
@@ -36,10 +40,13 @@ use function tribe_update_event;
 use function tribe_update_organizer;
 use function tribe_update_venue;
 use function var_dump;
-use function wp_create_nonce;
 use function wp_get_post_terms;
 use function wp_rand;
 use function wp_set_post_terms;
+use const CURLOPT_POST;
+use const CURLOPT_POSTFIELDS;
+use const CURLOPT_RETURNTRANSFER;
+use const CURLOPT_URL;
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
@@ -375,7 +382,7 @@ class The_Event_Calendar {
 			);
 			// General Datas
 			// retrieve event keywords
-			$keywords = wp_get_post_terms( $post_id, 'openagenda_keyword' );
+			$keywords = wp_get_post_terms( $post_id, 'post_tag' );
 			if ( ! empty( $keywords ) ) {
 				$keys = array();
 				foreach ( $keywords as $keyword ) {
@@ -413,30 +420,71 @@ class The_Event_Calendar {
 					]
 			];
 
+			$registration = esc_url_raw( $_POST['EventURL'] );
+			$data['registration']  = [
+			        $registration,
+                ];
+			if ( 'suffix' === $_POST['EventCurrencyPosition'] ){
+			    $conditions = $_POST['EventCost'] . $_POST['EventCurrencySymbol'];
+            } else {
+				$conditions = $_POST['EventCurrencySymbol'] . $_POST['EventCost'];
+            }
+			$data['conditions']    =
+				[
+					$locale => $conditions,
+				];
+
 			$location = tribe_get_venue_id( $post_id );
 			if ( ! empty( $location ) ) {
-				$location_id = get_post_meta( $location, '_oa_event_uid', true );
+				$location_id           = get_post_meta( $location, '_oa_event_uid', true );
+				$data['locationUid'] = intval( $location_id );
 			}
 
 			// get date
-			$dates = [
-				'start_date' => sanitize_text_field( $_POST['EventStartDate'] ),
-				'start_time' => sanitize_text_field( $_POST['EventStartTime'] ),
-				'end_time'   =>
-					sanitize_text_field( $_POST['EventEndTime'] ),
-				'end_date'   => sanitize_text_field( $_POST['EventEndDate'] ),
-				'tz'         => sanitize_text_field( $_POST['EventTimezone'] ),
 
-			];
+			$start_date = strtotime( $_POST['EventStartDate'] . $_POST['EventStartTime'] );
+			$end_date = strtotime( $_POST['EventEndDate'] .  $_POST['EventEndTime']);
+			$diff = intval( floor(( $end_date - $start_date ) / 86400 ) );
+			$end = strtotime( $_POST['EventStartDate'] . $_POST['EventEndTime'] );
 
-			$tz = self::get_time_zone( $dates );
+			$tz = self::get_time_zone( $start_date );
 
 			$i = 0;
-			foreach ( $dates as $date ) {
-
+			while ( $i <= $diff ){
+				$date['begin'] = $start_date + ($i * DAY_IN_SECONDS);
+				$date['end'] = $end + ($i * DAY_IN_SECONDS);
 				$timings[ $i ]['begin'] = date( "Y-m-d\TH:i:00$tz", $date['begin'] );
 				$timings[ $i ]['end']   = date( "Y-m-d\TH:i:00$tz", $date['end'] );
 				$i ++;
+			}
+
+			$data['timings'] = $timings;
+
+			$data['image'] = [
+			        'url' => get_the_post_thumbnail_url( $event->ID ),
+            ];
+
+			$posted = array(
+				'access_token' => $access_token,
+				'nonce'        => rand(),
+				'data'         => json_encode( $data ),
+				'lang'         => $locale,
+			);
+			$ch = curl_init();
+
+			curl_setopt( $ch, CURLOPT_URL, $route );
+			curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
+			curl_setopt( $ch, CURLOPT_POST, true );
+			curl_setopt( $ch, CURLOPT_POSTFIELDS, $posted );
+
+			$received_content = curl_exec( $ch );
+
+			$decode = json_decode( $received_content, true );
+
+			// update event uid
+			$uid = intval( $decode['event']['uid'] );
+			if ( $uid ) {
+				add_post_meta( $event->ID, '_oa_event_uid', $uid );
 			}
 		}
 	}
