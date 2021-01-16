c1c125f8dd7576c5a3a291486d11b851fb1c927e
Sébastien SERRE <sebastien@thivinfo.com>
2021-01-16 10:33:41 +0100

    load Carbon fields

--- 3rd-party/vendor/composer/autoload_static.php
+++ 3rd-party/vendor/composer/autoload_static.php
@@ -6,6 +6,30 @@ namespace Composer\Autoload;
 
 class ComposerStaticInit4794e46b2995c15720872b9f9f4d6935
 {
+    public static $prefixLengthsPsr4 = array (
+        'C' => 
+        array (
+            'Composer\\Installers\\' => 20,
+            'Carbon_Fields_Plugin\\' => 21,
+            'Carbon_Fields\\' => 14,
+        ),
+    );
+
+    public static $prefixDirsPsr4 = array (
+        'Composer\\Installers\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/composer/installers/src/Composer/Installers',
+        ),
+        'Carbon_Fields_Plugin\\' => 
+        array (
+            0 => __DIR__ . '/../..' . '/wp-content/plugins/carbon-fields-plugin/core',
+        ),
+        'Carbon_Fields\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/htmlburger/carbon-fields/core',
+        ),
+    );
+
     public static $prefixesPsr0 = array (
         'P' => 
         array (
@@ -19,6 +43,8 @@ class ComposerStaticInit4794e46b2995c15720872b9f9f4d6935
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
+            $loader->prefixLengthsPsr4 = ComposerStaticInit4794e46b2995c15720872b9f9f4d6935::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInit4794e46b2995c15720872b9f9f4d6935::$prefixDirsPsr4;
             $loader->prefixesPsr0 = ComposerStaticInit4794e46b2995c15720872b9f9f4d6935::$prefixesPsr0;
 
         }, null, ClassLoader::class);
