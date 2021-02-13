252de8132d62d9ff8a5c047b066a35228af62369
Sébastien SERRE <sebastien@thivinfo.com>
2021-02-13 17:24:02 +0100

    rm TEC settings

--- admin/register-settings.php
+++ admin/register-settings.php
@@ -253,11 +253,6 @@ function openwp_pro_register_settings() {
 	add_settings_field( 'openagenda-wp-sync', __( 'Force Import', 'wp-openagenda' ), 'openwp_oa_sync', 'openagenda-wp',
 		'openagenda-wp' );
 
-	//add_settings_section( 'openagenda-wp-3rd', __( 'The Event Calendar', 'wp-openagenda' ), '', 'openagenda-wp-3rd' );
-	register_setting( 'openagenda-wp', 'openagenda-tec' );
-	add_settings_field( 'openagenda-tec', __( 'Use The Event Calendar ?', 'wp-openagenda' ), 'openwp_tec', 'openagenda-wp',
-		'openagenda-wp' );
-
 }
 
 function openwp_oa_sync() {
@@ -296,11 +291,4 @@ function openwp_oa_secret() {
     <p><?php printf( wp_kses( __( 'Send a mail to <a href="%s" >OpenAgenda</a>, and ask them to activate your secret key.', 'wp-openagenda' ), $allowed_html ), esc_url( $url ) ); ?></p>
 	<?php
 	do_action( 'openagenda_after_secret' );
-}
-
-function openwp_tec(){
-	$tec = get_option( 'openagenda-tec' );
-	?>
-    <input name="openagenda-tec" type="checkbox" value="yes" <?php checked( 'yes', $tec ); ?>>
-	<?php
 }
