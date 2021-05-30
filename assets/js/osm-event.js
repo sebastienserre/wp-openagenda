94144992395b081f9fab6bbc3d8be757f566a535
Sébastien SERRE <sebastien@serree.com>
2021-05-30 13:03:42 +0200

    start a new day

--- assets/js/osm-event.js
+++
@@ -1,9 +0,0 @@
-/*
-jQuery(function($) {
-    var map = L.map('mapid').setView([ 48.856614,  2.352222], 13);
-    L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
-        attribution: '© <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> © <a href="https://carto.com/attributions">CARTO</a>',
-        subdomains: 'abcd',
-        maxZoom: 19,
-    }).addTo(map);
-});*/
